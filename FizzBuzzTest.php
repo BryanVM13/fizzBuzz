b41af37416d1341f0b3c55970a6de22cd4799cd5
unknown <bryan.vanmoer@student.vinci.be>
2020-10-26 17:21:52 +0100

    add - count for input 1

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -1,9 +1,22 @@
 <?php
 
+require "vendor/autoload.php";
+require "FizzBuzz.php";
 
 use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
+    public function test_count_given1_shouldReturn1(){
 
+
+        //Arrange = Preparer tout ce qu'on a besoin pour ecrire notre test
+        $fizzBuzz = new FizzBuzz();
+
+        //Act = executer la fonction qu'on souhaite tester
+        $actual = $fizzBuzz->count(1);
+
+        //Assert
+        $this->assertEquals("1",$actual);
+    }
 }
